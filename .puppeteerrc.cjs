ea0c90beafa98c3fd834bfe857fe95746d03f877
maliko917@gmail.com <21983103Malikov>
2022-12-26 00:29:18 +0100

    fix: Environment fix for puppeteer for HEROKU

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -3,6 +3,13 @@ const { join } = require('path');
 
 const isHeroku = process.env.BOT_ENVIRONMENT === 'heroku';
 
-module.exports = isHeroku
+console.log(process.env.BOT_ENVIRONMENT, 'process.env.BOT_ENVIRONMENT')
+console.log(isHeroku, 'isHeroku')
+
+module.exports = { cacheDirectory: join(__dirname, '.cache', 'puppeteer') }
+
+const res = isHeroku
     ? { cacheDirectory: join(__dirname, '.cache', 'puppeteer') }
     : { };
+
+console.log(res, 'res')
--- server.js
+++ server.js
@@ -1,19 +1,44 @@
 const express = require("express");
 const puppeteer = require("puppeteer");
+const { join } = require('path');
 
 const app = express();
 app.set("port", process.env.PORT || 5000);
-
-const browserP = puppeteer.launch({
-    args: ["--no-sandbox", "--disable-setuid-sandbox"]
-});
 
 app.get("/", (req, res) => {
     res.send(`<p>Our simple UI is here. Right now its ${Date()}</p>`);
 });
 
+app.get("/test", (req, res) => {
+    const isHeroku = process.env.BOT_ENVIRONMENT === 'heroku';
+
+    console.log(process.env.BOT_ENVIRONMENT, 'process.env.BOT_ENVIRONMENT')
+    console.log(isHeroku, 'isHeroku')
+
+    console.log(process.env, 'process.env')
+
+    const result = isHeroku
+        ? { cacheDirectory: join(__dirname, '.cache', 'puppeteer') }
+        : { };
+
+    console.log(res, 'res')
+
+    res.send(`
+        <p>Our simple UI is here. Right now its ${Date()}</p>
+        <div>${process.env.BOT_ENVIRONMENT} process.env.BOT_ENVIRONMENT</div>
+        <div>${isHeroku} isHeroku</div>
+        <div>${result} result</div>
+        <div>${process.env} process.env</div>
+    `);
+});
+
 app.get("/check-puppeteer", (req, res) => {
     let page;
+
+    const browserP = puppeteer.launch({
+        args: ["--no-sandbox", "--disable-setuid-sandbox"]
+    });
+
     (async () => {
         page = await (await browserP).newPage();
         await page.setContent(`<p>Puppeteer is working at ${Date()}</p>`);
