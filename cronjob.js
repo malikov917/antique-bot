3d0fcb3fc62457b34c898d8ba289a4068c794e92
maliko917@gmail.com <21983103Malikov>
2023-04-26 14:20:51 +0200

    added: cron job is fixed

--- cronjob.js
+++ cronjob.js
@@ -4,6 +4,8 @@ const {connectionSettings} = require("./configs/mongodb-connection-settings");
 require('dotenv').config();
 const CronJob = require('cron').CronJob;
 const shortReadService = new ShortReadService();
+const axios = require('axios');
+const HEROKU_APP_URL = 'https://antique-node-bot.herokuapp.com/';
 
 
 async function foo() {
@@ -15,9 +17,14 @@ const jobNews = new CronJob('0 9,12,15,17,19,21 * * *', function() {
   foo();
 }, null, true, 'UTC');
 
-// cronjob for heroku which is in UTC time and executes every 1 minute
-const keepAwakeJob = new CronJob('0,20,40 * * * *', function() {
+const keepAwakeJob = new CronJob('0,20,40 * * * *', async function () {
   console.log('[cron] keep awake: ', new Date().getHours(), new Date().getMinutes());
+  const res = await axios.get(HEROKU_APP_URL);
+  if (res.status === 200) {
+    console.log('app is running, req status 200');
+  } else {
+    console.log('app is not running, req status: ', res.status)
+  }
 }, null, true, 'UTC');
 
 mongoose.connect(process.env.ANTIQUE_DB_STRING, connectionSettings)
